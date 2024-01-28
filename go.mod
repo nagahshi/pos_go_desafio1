52abc41a0d0bb90449ab131ec82fc3f7cfdeed7a
nagahshi <willian.empari@gmail.com>
2024-01-28 20:30:29 -0300

    feat: criado nova conexão com o banco de dados

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nagahshi/pos_go_desafio1
+
+go 1.20
+
+require github.com/mattn/go-sqlite3 v1.14.20 // indirect
--- server/db/sqlite/sqlite.go
+++ server/db/sqlite/sqlite.go
@@ -1,1 +1,24 @@
 package sqlite
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// NewConnDB - cria uma nova conexão com o banco de dados SQLite.
+func NewConnDB(nameDB string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", nameDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Verifica a conexão com o banco de dados.
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, nil
+}
